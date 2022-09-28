1a2e254b6492dfc1a6c3b00a67eed9aa4fd15de3
serokrates <homm5@vp.pl>
2022-09-28 15:14:46 +0200

    serve production

--- backend/server.js
+++ backend/server.js
@@ -16,17 +16,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/messages", require("./routes/messageRoutes"));
 
 // Serve frontend
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../frontend/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(
-//       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-//     )
-//   );
-// } else {
-//   app.get('/', (req, res) => res.send('Please set to production'));
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
 app.use(errorHandler);
 
